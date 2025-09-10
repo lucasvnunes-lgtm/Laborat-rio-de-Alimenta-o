85a9aed51999a938cca4656a905d11034856e9f7
Felipe <felipegustavo042009@gmail.com>
2025-09-10 11:22:15 -0300

    Delete atividade directory

--- atividade/at.js
+++
@@ -1,8 +0,0 @@
-const valorMultiplo=2;
-
-function valorFinal(){
-    let valor = (document.getElementById("Pegarvalor").value);
-    let multiplo = valor * valorMultiplo;
-    document.getElementById("valor").innerText=multiplo
-    console.log(multiplo)
-}
