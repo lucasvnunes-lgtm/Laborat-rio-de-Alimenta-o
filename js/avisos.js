c970da86a0b3675a84a3a84393fb9da939452067
Felipe <felipegustavo042009@gmail.com>
2025-09-10 11:30:13 -0300

    Avisos adicionados
    Coloquei um sistema de avisos só falatando a parte visual

---
+++ js/avisos.js
@@ -0,0 +1,38 @@
+export let aviso = [
+    {
+        nome:"Troca de espetor",
+        data: "09/09/2024",
+        importancia: "alta/baixa",
+        informacao: "Falta 33 de espetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "09/09/2025",
+        importancia: "alta/baixa",
+        informacao: "Falta de ispetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "01/09/2025",
+        importancia: "alta/baixa",
+        informacao: "Falta de espetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "01/10/2025",
+        importancia: "alta/baixa",
+        informacao: "Falta de espetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "09/09/2025",
+        importancia: "alta/baixa",
+        informacao: "Falta de espetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "09/09/2025",
+        importancia: "alta/baixa",
+        informacao: "Falta de espetor no dia 24"
+    }
+];
