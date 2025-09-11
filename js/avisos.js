97cd33eb261d9ce4cfb97a8128328a992408baae
Felipe <felipegustavo042009@gmail.com>
2025-09-10 22:52:51 -0300

    Sem adm
    Tirei o adm, tirei umas cisas do index

---
+++ js/avisos.js
@@ -0,0 +1,38 @@
+export let aviso = [
+    {
+        nome:"Troca de espetor",
+        data: "07/09/2025",
+        importancia: "alerta",
+        informacao: "Falta 33 de espetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "08/09/2025",
+        importancia: "novidade",
+        informacao: "Falta de ispetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "07/09/2025",
+        importancia: "novidade",
+        informacao: "Falta de espetor no dia 24"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "09/10/2025",
+        importancia: "novidade",
+        informacao: "Falta de espetor no dia 23"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "07/09/2025",
+        importancia: "alerta",
+        informacao: "Falta de espetor no dia 1"
+    },
+    {
+        nome:"Troca de espetor",
+        data: "09/09/2025",
+        importancia: "alerta",
+        informacao: "Falta de espetor no dia 4"
+    }
+];
