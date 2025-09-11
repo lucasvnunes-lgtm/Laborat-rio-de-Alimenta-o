cfd03096ab03b94e9afe3d3dfaaad80de5c1a93f
Felipe <felipegustavo042009@gmail.com>
2025-09-11 16:43:28 -0300

    na
    Novas imagens para colocar e um sistema d eavisos com avisos quase reais

---
+++ js/proutos.js
@@ -0,0 +1,172 @@
+//Isso aqui é pra aparecer os nomes dos produtos lá na seção lanches
+
+export let salgado = [
+    {
+        url: "images/produto/coxinha.png",
+        nome: "Coxinha",
+        infor: "café e agua",
+        preco: 6
+    },
+
+    {
+        url: "images/produto/enroladinho_salsicha.png",
+        nome: "Enroladinho de Salsicha",
+        infor: "café e agua",
+        preco: 6
+    },
+
+    {
+        url: "images/produto/pao_recheado.png",
+        nome: "Pão Recheado/Folhado",
+        preco: 7
+    },
+
+   {
+     url: "images/produto/pao_recheado.png",
+     nome: "Pão Recheado/Folhado",
+     preco: 7
+   }
+];
+
+
+
+// Doces e Chocolates
+export let docesChocolate = [
+
+   {
+     url: "images/produto/sorvetes.jpg",
+    nome: "BIS Xtra",
+     preco: 5
+   },
+
+  {
+    url:"images/produto/sorvetes.jpg",
+   nome: "KitKat",
+    preco: 6
+  },
+
+   {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Serenata de Amor",
+     preco: 2.5
+   },
+
+   {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Pirulitos Variados",
+     preco: 1.5
+   },
+
+   {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Doce de Corte",
+     preco: 4
+   },
+
+   {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Bolo",
+     preco: 5
+   }
+];
+
+// Gelados
+    export let gelados = [
+{
+    url: "images/produto/sorvetes.jpg",
+     nome: "Sorvetes",
+     preco: 10
+}];
+     
+
+// Bebidas
+export let bebidas = [
+
+ {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Coca-Cola Lata",
+     preco: 6
+ },
+
+{
+     url: "images/produto/sorvetes.jpg",
+     nome: "Fanta Laranja Lata",
+     preco: 6
+ },
+
+ {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Fanta Uva Lata",
+     preco: 6
+ },
+
+{
+     url: "images/produto/sorvetes.jpg",
+     nome: "Pepsi Lata",
+     preco: 6
+ },
+
+ {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Sprite Garrafa",
+     preco: 7
+ },
+{
+     url: "images/produto/sorvetes.jpg",
+     nome: "Guaramix",
+     preco: 5
+ },
+  {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Garrafa H2OH",
+     preco: 3
+ },
+  {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Água Mineral",
+     preco: 3
+ },
+ {
+     url: "images/produto/sorvetes.jpg",
+     nome: "Suco Maratinho",
+     preco: 5
+ }
+];
+
+
+
+ //  Salgadinhos
+ export let salgadinhos = [
+
+ {
+     url: "images/produto/salgadinhoQueijo.png",
+     nome: "Amendupã Snacks Queijo",
+     preco: 4
+ },
+
+ {
+     url: "images/produto/salgadinhoCalabreza.jpg",
+     nome: "Amendupã Snacks Calabresa",
+     preco: 4
+ },
+
+ {
+     url: "images/produto/solhit.png",
+     nome: "Sol Hits Requeijão",
+     preco: 3.5
+ },
+
+ {
+     url: "images/produto/solhit.png",
+     nome: "Sol Hits Queijo",
+     preco: 3.5
+ },
+
+ {
+     url: "images/produto/clube_social.png",
+     nome: "Club Social",
+     preco: 3
+ }
+
+];
+
