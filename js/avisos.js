6b5e9257e54c447a78984304881c696ad4d97f52
Felipe <felipegustavo042009@gmail.com>
2025-09-13 22:24:45 -0300

    Add files via upload

---
+++ js/avisos.js
@@ -0,0 +1,14 @@
+export let aviso = [
+  {
+    nome: "Inaguração do site",
+    data: "10/09/2025",
+    importancia: "alerta",
+    informacao: "Lançamento do novo site para a cantina. Menu online, avisos e contatos."
+  },
+  {
+    nome: "Desafio: Apl1Mate",
+    data: "11/09/2025",
+    importancia: "important",
+    informacao: "Competição para construir a ponte mais resistente usando macarrão. Objetivo: suportar o maior peso possível."
+  }
+];
---
+++ js/proutos.js
@@ -0,0 +1,158 @@
+//Isso aqui é pra aparecer os nomes dos produtos lá na seção lanches
+
+export let salgado = [
+    {
+        url: "images/produto/coxinha.png",
+        nome: "Coxinha",
+        preco: 6
+    },
+    {
+        url: "images/produto/enroladinho_salsicha.png",
+        nome: "Enroladinho",
+        preco: 6
+    },
+    {
+        url: "images/produto/pao_recheado.png",
+        nome: "Pão Folhado",
+        preco: 7
+    },
+    {
+        url: "images/produto/kibe.jpg",
+        nome: "Kibe",
+        preco: 6 // novo salgado adicionado
+    }
+];
+
+
+// Doces e Chocolates
+export let docesChocolate = [
+
+    {
+        url: "images/produto/bix.png",
+        nome: "BIS Xtra",
+        preco: 5
+    },
+
+    {
+        url: "images/produto/kit_kat.png",
+        nome: "KitKat",
+        preco: 6
+    },
+
+    {
+        url: "images/produto/pasoca.png",
+        nome: "Paçoquita",
+        preco: 2.5
+    },
+
+    {
+        url: "images/produto/pirulito.png",
+        nome: "Pirulitos",
+        preco: 1.5
+    },
+
+    {
+        url: "images/produto/rapadura.png",
+        nome: "Doce de Corte",
+        preco: 4
+    },
+
+    {
+        url: "images/produto/bolo.png",
+        nome: "Bolo",
+        preco: 5
+    }
+];
+
+// Gelados
+export let gelados = [
+    {
+        url: "images/produto/sorvetes.jpg",
+        nome: "Sorvetes",
+        preco: 10
+    }];
+
+
+// Bebidas
+export let bebidas = [
+
+    {
+        url: "images/produto/coca.png",
+        nome: "Coca-Cola",
+        preco: 6
+    },
+
+    {
+        url: "images/produto/fanta_laranja.png",
+        nome: "Fanta Laranja",
+        preco: 6
+    },
+
+    {
+        url: "images/produto/fanta_uva.jpg",
+        nome: "Fanta Uva",
+        preco: 6
+    },
+
+    {
+        url: "images/produto/pepsi.png",
+        nome: "Pepsi",
+        preco: 6
+    },
+
+   
+    {
+        url: "images/produto/guaramix.png",
+        nome: "Guaramix",
+        preco: 5
+    },
+    {
+        url: "images/produto/H2O.png",
+        nome: "H2OH",
+        preco: 3
+    },
+    {
+        url: "images/produto/agua.png",
+        nome: "Água Mineral",
+        preco: 3
+    },
+    {
+        url: "images/produto/maratinho.png",
+        nome: "Maratinho",
+        preco: 5
+    }
+];
+
+
+
+//  Salgadinhos
+export let salgadinhos = [
+
+    {
+        url: "images/produto/salgadinhoQueijo.png",
+        nome: "Amendupã Queijo",
+        preco: 4
+    },
+
+    {
+        url: "images/produto/salgadinhoCalabreza.jpg",
+        nome: "Amendupã Calabresa",
+        preco: 4
+    },
+
+
+    {
+        url: "images/produto/solhit.png",
+        nome: "Hits Queijo",
+        preco: 3.5
+    },
+
+    {
+        url: "images/produto/clube_social.png",
+        nome: "Club Social",
+        preco: 3
+    }
+
+];
+
+
